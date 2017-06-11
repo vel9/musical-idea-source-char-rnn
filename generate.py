97a1091b702531b7bdefa0dbb4922f344f0c1726
Levan Iordanishvili <levandev9053@gmail.com>
2017-06-11 11:37:29 -0400

    clean up the sampling and sample-to-usable-output phase

--- generate.py
+++ generate.py
@@ -1,15 +1,27 @@
 import glob
 import os.path
+import config
 
 from music21 import converter
 from shutil import copyfile
 from sample import sample_checkpoint
 from music21.humdrum.spineParser import SpineLine
 
+GENERETED_FILES_DIR = "generated-music"
+CHECKPOINTS_DIR = "checkpoints"
+
 def should_count_spines(line):
-	return line != "" and line != "@"
+	"""
+	Returns true if number of spines should be counted for current line
+	"""
+	return line != "" and line != config.MEASURE_SYMBOL
 
 def determine_num_voices(kern_content):
+	"""
+	Determines the number of voices present in the generated **kern file
+	by scanning each line and determining number of vocies in current line
+	relies on music21.humdrum.spineParser.SpineLine class
+	"""
 	max_voices = 0
 	line_num = 0
 	for line in kern_content.splitlines():
@@ -20,14 +32,22 @@ def determine_num_voices(kern_content):
 		line_num += 1
 	return max_voices
 
-# handles rests and measure numbers
 def preprocess(kern_content, max_voices):
+	"""
+	Translates the @ symbol back into the standard measure number,
+	ensures the the current line does not contain operations
+	which would throw off the parsers downstream, like Interpretation lines, 
+
+	see here for more detail on Interpretation lines and 
+	the organization of a **kern file in general: 
+	http://www.humdrum.org/guide/ch05/
+	"""
 	bar = 1
 	line_num = 0
 	updated_content = ""
 	for line in kern_content.splitlines():
 		line = line.strip()
-		if (line.startswith("@")):
+		if (line.startswith(config.MEASURE_SYMBOL)):
 			bar_content = copy_append("={}\t".format(bar), max_voices) + "\n"
 			updated_content += bar_content
 			bar += 1
@@ -40,22 +60,43 @@ def preprocess(kern_content, max_voices):
 	return updated_content
 
 def copy_append(value_to_append, num_copies):
+	"""
+	Copies and appends a value to itself num_copies times
+	"""
 	value = value_to_append
 	for i in range(1, num_copies):
 		value += value_to_append
 	return value
 
 def copy_append_staff(num_copies):
+	"""
+	Copies the staff keyword num_copies amount of times
+	Also adds the the staff number number for each keyword
+	"""
 	value = ""
 	for i in range(0, num_copies):
 		value += "*staff{}\t".format(str(i + 1))
 	return value
 
 def contains_illegal_operations(line):
-	# ref: http://stackoverflow.com/a/3437070
+	"""
+	Determines if current line contains operations
+	which would likely stump the parsing process, 
+	
+	More on "Interpretation" lines in **kern: http://www.humdrum.org/guide/ch05/
+
+	ref: http://stackoverflow.com/a/3437070
+	"""
 	return "*" in line or "v" in line
 
 def generate_krn(kern_content, output_file_name):
+	"""
+	Generates the metadata needed to successfully parse the 
+	**krn file, see the following resources: 
+
+	http://www.humdrum.org/guide/ch05/
+	http://www.wise.io/tech/asking-rnn-and-ltsm-what-would-mozart-write
+	"""
 	r = []
 	num_voices = determine_num_voices(kern_content)
 	kern_content = preprocess(kern_content, num_voices)
@@ -74,41 +115,58 @@ def generate_krn(kern_content, output_file_name):
 	r.append(copy_append("== ", num_voices) + "\n")
 	r.append(copy_append("*- ", num_voices) + "\n")
 	open(output_file_name,"w").writelines(r)
-	print('finished writing krn file')
+	print("finished writing krn file")
 
 def generate_midi(music_score, output_midi_filename):
-	midi_file = music_score.write('midi')
+	"""
+	Converts MusicScore object to midi file
+	"""
+	midi_file = music_score.write("midi")
 	copyfile(midi_file, output_midi_filename)
-	print('finished writing midi file', midi_file)
+	print("finished writing midi file", midi_file)
 
 def generate_xml(music_score, output_xml_filename):
-	mxl_file = music_score.write('musicxml')
+	"""
+	Converts MusicScore object to xml file
+	"""
+	mxl_file = music_score.write("musicxml")
 	copyfile(mxl_file, output_xml_filename)
-	print('finished writing musicxml file', mxl_file)
+	print("finished writing musicxml file", mxl_file)
 
 def generate_files(raw_krn_content, output_krn_filename, output_midi_filename, output_xml_filename):
+	"""
+	Generates the parsable krn file from RNN's raw output
+
+	Once the **kern file is parsed, it is coverted to midi and xml formats
+	"""
 	generate_krn(raw_krn_content, output_krn_filename)
 	m = converter.parse(output_krn_filename)
 	generate_midi(m, output_midi_filename)
 	generate_xml(m, output_xml_filename)
 
-def generate(model_version, checkpoint_name, num_chars, lstm_size, prime="@"):
-	# build file paths
-	generated_dir = 'generated-music/'
-	output_krn_dir = generated_dir + model_version + '/krn'
-	output_midi_dir = generated_dir + model_version + '/midi'
-	output_xml_dir = generated_dir + model_version + '/xml'
-	output_base_filename = 'gen' + str(len(os.listdir(output_krn_dir)) + 1)
-	output_krn_filename = output_krn_dir + '/' + output_base_filename + '.krn'
-	output_midi_filename = output_midi_dir + '/' + output_base_filename + '.mid'
-	output_xml_filename = output_xml_dir + '/' + output_base_filename + '.xml'
-	# build checkpoint dir
-	checkpoint_dir = 'checkpoints/' + model_version
+def generate(model_version, checkpoint_name, num_chars, lstm_size, prime=config.MEASURE_SYMBOL):
+	"""
+	Sample the RNN by providing a path to an existing checkpoint
+	Use the RNN output to generate the midi and xml files from it 
+	"""
+	generated_dir = GENERETED_FILES_DIR + "/" + model_version
+	output_krn_dir = generated_dir + "/krn"
+	output_midi_dir = generated_dir + "/midi"
+	output_xml_dir = generated_dir + "/xml"
+	
+	# count the number of files in director and add one 
+	output_base_filename = "gen" + str(len(os.listdir(output_krn_dir)) + 1)
+
+	output_krn_filename = output_krn_dir + "/" + output_base_filename + ".krn"
+	output_midi_filename = output_midi_dir + "/" + output_base_filename + ".mid"
+	output_xml_filename = output_xml_dir + "/" + output_base_filename + ".xml"
+	checkpoint_dir = CHECKPOINTS_DIR + "/" + model_version
 
 	print("sampling from model, and generating new music, base file name {}: ".format(output_base_filename))
 	raw_krn_content = sample_checkpoint(checkpoint_dir, checkpoint_name, num_chars, lstm_size, prime)
 	generate_files(raw_krn_content, output_krn_filename, output_midi_filename, output_xml_filename)
 	
 if __name__ == "__main__":
-	starting_event = "@\n.\t.\t2a/\t.\n"
-	generate('v24', '/trained.ckpt', 3000, 2048, starting_event)
+	checkpoint_name = "/" + config.CHECKPOINT_NAME
+	generate(config.MODEL_VERSION, checkpoint_name, 
+		config.OUTPUT_LENGTH, config.LSTM_SIZE, config.PRIME_EVENT)
--- sample.py
+++ sample.py
@@ -1,26 +1,54 @@
+# -----------
 # This network is from Udacity's Intro to RNN project
-# https://github.com/udacity/deep-learning/tree/master/intro-to-rnns 
+# And was part of the assigment to build an RNN which
+# The Model in the assignment was originally trained on the text of Anna Karenina. 
+# 
+# Original model: 
+#   https://github.com/udacity/deep-learning/tree/master/intro-to-rnns
+# Original model inspired by Karpathy's Character RNN: 
+#   http://karpathy.github.io/2015/05/21/rnn-effectiveness/ 
+# ------------
 import time
 import glob
-
 import numpy as np
 import tensorflow as tf
+import config
 
 from collections import namedtuple
 
 from model import get_batches
 from model import CharRNN
-
 from data import read_processed_data
 
 def pick_top_n(preds, vocab_size, top_n=5):
+    """
+    Picks one of top_n most likely next characters, 
+    Random selection step is provided with the actual
+    probabilities with which to make the weighted
+    choice between the top_n characters.
+    """
     p = np.squeeze(preds)
-    p[np.argsort(p)[:-top_n]] = 0
-    p = p / np.sum(p)
-    c = np.random.choice(vocab_size, 1, p=p)[0]
+    indexes_to_retain = np.argsort(p)[-top_n:]
+    for i in range(0, len(p)):
+        if i not in indexes_to_retain:
+            p[i] = 0
+    probs = p / np.sum(p)
+    c = np.random.choice(vocab_size, 1, p=probs)[0]
     return c
 
-def sample(checkpoint, n_samples, lstm_size, processd_data_dir, prime="@"):
+def get_feed(model, x, new_state):
+    """
+    Short cut for creating the new feed dict for each character
+    """
+    return {model.inputs: x,
+            model.keep_prob: 1.,
+            model.initial_state: new_state}
+
+def sample(checkpoint, n_samples, lstm_size, processd_data_dir, prime=config.MEASURE_SYMBOL):
+    """
+    First primes the RNN with the prime event, 
+    Then continues generating next character for n_samples times
+    """
     vocab, vocab_to_int, int_to_vocab, encoded = read_processed_data(processd_data_dir)
     vocab_size = len(vocab)
 
@@ -30,31 +58,21 @@ def sample(checkpoint, n_samples, lstm_size, processd_data_dir, prime="@"):
     with tf.Session() as sess:
         saver.restore(sess, checkpoint)
         new_state = sess.run(model.initial_state)
-
         # prime the sample
         for c in prime:
             x = np.zeros((1, 1))
             x[0,0] = vocab_to_int[c]
-            feed = {model.inputs: x,
-                    model.keep_prob: 1.,
-                    model.initial_state: new_state}
             preds, new_state = sess.run([model.prediction, model.final_state], 
-                                         feed_dict=feed)
-
+                                         feed_dict=get_feed(model, x, new_state))
+        # continue generating 
         c = pick_top_n(preds, vocab_size)
         samples.append(int_to_vocab[c])
-
         for i in range(n_samples):
             x[0,0] = c
-            feed = {model.inputs: x,
-                    model.keep_prob: 1.,
-                    model.initial_state: new_state}
             preds, new_state = sess.run([model.prediction, model.final_state], 
-                                         feed_dict=feed)
-
+                                         feed_dict=get_feed(model, x, new_state))
             c = pick_top_n(preds, vocab_size)
             samples.append(int_to_vocab[c])
-        
     return ''.join(samples)
 
 def sample_checkpoint(checkpoint_dir, checkpoint, n_samples, lstm_size, prime):
