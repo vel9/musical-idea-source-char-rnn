c6848366b3f0c13ef7f86007d9722b0c859f43ca
Levan Iordanishvili <levandev9053@gmail.com>
2017-06-11 11:35:59 -0400

    clean up data processing phase

--- data.py
+++ data.py
@@ -1,100 +1,171 @@
-# This network is from Udacity's Intro to RNN project
-
-# data from https://github.com/automata/ana-music
 import glob
 import numpy as np
 import _pickle as pickle
 
 from pathlib import Path
 
-vocab_file_name = '/vocab.p'
-vocab_to_int_file_name = '/vocab_to_int.p'
-int_to_vocab_file_name = '/int_to_vocab.p'
-encoded_file_name = '/encoded.p'
+VOCAB_FILE_NAME = "vocab.p"
+VOCAB_TO_INT_FILE_NAME = "vocab_to_int.p"
+INT_TO_VOCAB_FILE_NAME = "int_to_vocab.p"
+ENCODED_VOCAB_FILE_NAME = "encoded.p"
+DATASET_DIR = "dataset"
+DATASET_FILENAME = "music.txt"
+# Provide list of composers whose music we'll be processing into the training data
+COMPOSERS = ["beethoven", "chopin", "haydn", "mozart", "prokofiev",
+"ravel", "scarlatti","schubert","scriabin"]
 
 def get_output_dir(args):
+    """
+    Provides the output directory for files generated from 
+    the data processing phase (pickle outputs of the processed/encoded dataset)
+    as well as the the training phase (the tensorflow checkpoints)
+
+    The "local" output is placed in the relative output directory 
+    Floydhub, on the other hand requires outputs to be placed in the "/output"
+    directory, as this is the "only path Floyd will preserver"
+
+    See more: http://docs.floydhub.com/commands/output/ 
+    """
     output_dir_dict = {"local": "output", "floyd": "/output"}
     if len(args) < 2:
         raise ValueError("please provide an env argument [local or floyd]")
     else:
         env_args = (args)[1]
-        output_dir = output_dir_dict[env_args]
-        return output_dir
+        return output_dir_dict[env_args]
 
-def perhaps_read(file_name, file_content):
+def read_file(file_path):
+    """
+    Loads pickle file from file_path, returns None if it doesn't exist
+    """
     try:
-        file_content =  pickle.load(open(file_name, "rb"))
-        print("loading {} from file-system".format(file_name))
+        file_content =  pickle.load(open(file_path, "rb"))
+        print("loading {} from file system".format(file_path))
         return file_content
     except (OSError, IOError) as e:
-        pickle.dump(file_content, open(file_name, "wb"))
-        print("saving new file: {} to file-system".format(file_name))
-        return file_content
+        print("No file found at {}".format(file_path))
+        return None
 
-def read_file(file_name):
-    return pickle.load(open(file_name, "rb"))
+def write_file(file_path, file_content):
+    """
+    Writes file content to file_path
+    """
+    pickle.dump(file_content, open(file_path, "wb"))
+    print("saving new file: {} to file system".format(file_path))
 
 def read_processed_data(directory):
-    vocab = read_file(directory + vocab_file_name)
-    vocab_to_int = read_file(directory + vocab_to_int_file_name)
-    int_to_vocab = read_file(directory + int_to_vocab_file_name)
-    encoded = read_file(directory + encoded_file_name)
+    """
+    Reads already-processed files from file system
+    """
+    directory = directory + "/"
+    vocab = read_file(directory + VOCAB_FILE_NAME)
+    vocab_to_int = read_file(directory + VOCAB_TO_INT_FILE_NAME)
+    int_to_vocab = read_file(directory + INT_TO_VOCAB_FILE_NAME)
+    encoded = read_file(directory + ENCODED_VOCAB_FILE_NAME)
     return vocab, vocab_to_int, int_to_vocab, encoded
 
+def generate_vocab(output_dir, text):
+    """
+    Generates a unique set of all characters found in the dataset text
+    """
+    vocab_file_path = output_dir + VOCAB_FILE_NAME
+    vocab = read_file(vocab_file_path)
+    if (vocab is None):
+        vocab = set(text)
+        write_file(vocab_file_path, vocab)
+    return vocab
+
+def generate_vocab_to_int(output_dir, vocab):
+    """
+    Maps each character to an integer value: {"b":1}
+    """
+    vocab_to_int_file_path = output_dir + VOCAB_TO_INT_FILE_NAME
+    vocab_to_int = read_file(vocab_to_int_file_path)
+    if (vocab_to_int is None):
+        vocab_to_int = {c: i for i, c in enumerate(vocab)}
+        write_file(vocab_to_int_file_path, vocab_to_int)
+    return vocab_to_int
+
+def generate_int_to_vocab(output_dir, vocab_to_int):
+    """
+    Crates a reverse mapping from vocab_to_int
+    ref: https://stackoverflow.com/a/2569074
+    """
+    int_to_vocab_file_path = output_dir + INT_TO_VOCAB_FILE_NAME
+    int_to_vocab = read_file(int_to_vocab_file_path)
+    if (int_to_vocab is None):
+        int_to_vocab = {v: k for k, v in vocab_to_int.items()}
+        write_file(int_to_vocab_file_path, int_to_vocab)
+    return int_to_vocab
+
+def generate_encoded(output_dir, vocab_to_int, text):
+    """
+    Encodes the input dataset text into integers as mapped in vocab_to_int
+    Sequence "bbc" would become [1,1,2]
+    """
+    encoded_file_path = output_dir + ENCODED_VOCAB_FILE_NAME
+    encoded = read_file(encoded_file_path)
+    if (encoded is None):
+        encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
+        write_file(encoded_file_path, encoded)
+    return encoded
+
 def process(text, output_dir):
-    vocab = perhaps_read(output_dir + vocab_file_name, set(text))
-    vocab_to_int = {c: i for i, c in enumerate(vocab)}
-    vocab_to_int = perhaps_read(output_dir + vocab_to_int_file_name, vocab_to_int)
-    int_to_vocab = perhaps_read(output_dir + int_to_vocab_file_name, dict(enumerate(vocab)))
-    encoded = np.array([vocab_to_int[c] for c in text], dtype=np.int32)
-    encoded = perhaps_read(output_dir + encoded_file_name, encoded)
+    """
+    Processes input data into data structures needed for the training process
+    """
+    output_dir = output_dir + "/"
+    vocab = generate_vocab(output_dir, text)
+    vocab_to_int = generate_vocab_to_int(output_dir, vocab)
+    int_to_vocab = generate_int_to_vocab(output_dir, vocab_to_int)
+    encoded = generate_encoded(output_dir, vocab_to_int, text)
     return vocab, vocab_to_int, int_to_vocab, encoded
 
 def generate_data(output_dir):
-    dataset = 'dataset2'
-    ddataset_file_name = 'music.txt'
-    music_txt = Path(ddataset_file_name)
+    """
+    Processing code primarily adopted from: 
+    http://www.wise.io/tech/asking-rnn-and-ltsm-what-would-mozart-write
+
+    Reads **kern files from a local dataset, the files are placed within 
+    folders named by composer names: dataset/mosart/[mozart's compositions]
+    
+    Crucially, the process replaces measure numbers in the **kern file with the
+    @ symbol, hiding the measure numbers itself from the training process. 
+
+    If the generated dataset already exists, use it
+    """
+    dataset_dir = DATASET_DIR
+    dataset_file_name = DATASET_FILENAME
+    music_txt = Path(dataset_file_name)
     if music_txt.is_file():
-        with open(datase_file_name, 'r') as existing_data_file:
+        with open(dataset_file_name, "r") as existing_data_file:
             existing_data = existing_data_file.read()
             if (len(existing_data) > 0):
                 print("found existing dataset...")
                 return process(existing_data, output_dir)
 
     print("generating dataset...")
-    composers = ["beethoven",
-                "chopin",
-                "haydn",
-                "mozart",
-                "prokofiev",
-                "ravel",
-                "scarlatti",
-                "schubert",
-                "scriabin"]
-
-    comp_txt = open(ddataset_file_name, "w")
+    comp_txt = open(dataset_file_name, "w")
     # ref: http://www.wise.io/tech/asking-rnn-and-ltsm-what-would-mozart-write
     # get the top directory ites
-    for composer in composers:
-        ll = glob.glob(dataset + '/{composer}/*.krn'.format(composer=composer))
-        for song in ll:
-            lines = open(song,"r").readlines()
+    for composer in COMPOSERS:
+        compositions = glob.glob(dataset_dir + "/{composer}/*.krn".format(composer=composer))
+        for composition in compositions:
+            lines = open(composition, "r").readlines()
             out = []
-            found_first = False
+            found_first_measure = False
             for l in lines:
                 if l.startswith("="):
-                    ## new measure, replace the measure with the @ sign
-                    out.append("@\n")
-                    found_first = True
+                    out.append(config.MEASURE_SYMBOL + "\n")
+                    found_first_measure = True
                     continue
-                if not found_first or l.startswith("!"):
-                    ## keep going until we find the end of the header and metadata
+                if not found_first_measure or l.startswith("!"):
+                    ## skip line until we find the end of the header and metadata
                     ## and ignore comments
                     continue
                 out.append(l)
             comp_txt.writelines(out)
     comp_txt.close()
 
-    with open(datase_file_name, 'r') as f:
+    with open(dataset_file_name, "r") as f:
         text = f.read()
     return process(text, output_dir)
