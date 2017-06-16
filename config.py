3917d33215761871ea8ee31851b5b2f5963f0038
Levan Iordanishvili <levandev9053@gmail.com>
2017-06-16 18:39:14 -0400

    clean up config file and remove unused test_generate.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ Training: hyper-parameters for training the model
 """
 BATCH_SIZE = 64         # Sequences per batch
 NUM_STEPS = 150         # Number of sequence steps per batch
-LSTM_SIZE = 128         # Size of hidden layers in LSTMs
+LSTM_SIZE = 2048        # Size of hidden layers in LSTMs
 NUM_LAYERS = 2          # Number of LSTM layers
 LEARNING_RATE = 0.001   # Learning rate
 KEEP_PROB = 0.5         # Dropout keep probability
@@ -14,7 +14,7 @@ EPOCHS = 60
 Sampling: params for sampling the model
 """
 OUTPUT_LENGTH = 1000
-MODEL_VERSION = "v25"
+MODEL_VERSION = "v24"
 CHECKPOINT_NAME = "trained.ckpt"
 PRIME_EVENT = "@\n.\t.\t2a/\t.\n"
 
