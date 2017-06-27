acbf9cf88ef82ae0affd2a73e68560ecd630672b
Levan Iordanishvili <levandev9053@gmail.com>
2017-06-27 08:14:56 -0400

    updated hyperparameters

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ NUM_STEPS = 100         # Number of sequence steps per batch
 LSTM_SIZE = 512         # Size of hidden layers in LSTMs
 NUM_LAYERS = 2          # Number of LSTM layers
 LEARNING_RATE = 0.001   # Learning rate
-KEEP_PROB = 0           # Dropout keep probability
+KEEP_PROB = 1.0         # Dropout keep probability
 SAVE_EVERY_N = 5000     # Save every N iterations
 EPOCHS = 60
 
 """
 Sampling: params for sampling the model
 """
-OUTPUT_LENGTH = 3000
+OUTPUT_LENGTH = 1000
 MODEL_VERSION = "v25"
 CHECKPOINT_NAME = "trained.ckpt"
-PRIME_EVENT = "@\n.\t8ee-/L\n"
+PRIME_EVENT = "@\n8a"
 #Picks one of top_n most likely next characters
 PICK_TOP_N = 5
 
